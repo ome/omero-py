9c66fd111fd08e4d661dd0274e68f4fec888e986
William Moore <w.moore@dundee.ac.uk>
2019-09-05 12:13:39 +0100

    Add methods to test BlitzGateway unicode handling

--- test/unit/test_gateway.py
+++ test/unit/test_gateway.py
@@ -27,8 +27,12 @@ Test of various things under omero.gateway
 import Ice
 import pytest
 
-from omero.gateway import BlitzGateway, ImageWrapper
-from omero.model import ImageI, PixelsI, ExperimenterI, EventI
+from omero.gateway import BlitzGateway, ImageWrapper, \
+    ExperimenterWrapper, ProjectWrapper, AnnotationWrapper, \
+    PlateWrapper, WellWrapper
+from omero.model import ImageI, PixelsI, ExperimenterI, EventI, \
+    ProjectI, TagAnnotationI, FileAnnotationI, OriginalFileI, \
+    MapAnnotationI, NamedValue, PlateI, WellI
 from omero.rtypes import rstring, rtime, rlong, rint
 
 
@@ -64,6 +68,99 @@ def wrapped_image():
     creation_event.time = rtime(2000)  # In milliseconds
     image.details.creationEvent = creation_event
     return ImageWrapper(conn=MockConnection(), obj=image)
+
+
+class TestObjectsUnicode(object):
+    """
+    Tests that ExperimenterWrapper methods return correct unicode
+    """
+
+    def test_experimenter(self):
+        """
+        Tests methods handled by BlitzObjectWrapper.__getattr__
+
+        These will return unicode strings
+        """
+        first_name = u'fîrst_nąmę'
+        last_name = u'làst_NÅMÉ'
+        experimenter = ExperimenterI()
+        experimenter.firstName = rstring(first_name)
+        experimenter.lastName = rstring(last_name)
+
+        exp = ExperimenterWrapper(None, experimenter)
+        assert exp.getFirstName() == first_name
+        assert exp.getLastName() == last_name
+        assert exp.getFullName() == "%s %s" % (first_name, last_name)
+
+    def test_project(self):
+        """Tests BlitzObjectWrapper.getName() returns string"""
+        name = u'Pròjëct ©ψ'
+        desc = u"Desc Φωλ"
+        project = ProjectI()
+        project.name = rstring(name)
+        project.description = rstring(desc)
+
+        proj = ProjectWrapper(None, project)
+        assert proj.getName() == name.encode('utf8')
+        assert proj.getDescription() == desc.encode('utf8')
+
+    def test_tag_annotation(self):
+        """Tests AnnotationWrapper methods return strings"""
+        ns = u'πλζ.test.ζ'
+        text_value = u'Tαg - ℗'
+        obj = TagAnnotationI()
+        obj.textValue = rstring(text_value)
+        obj.ns = rstring(ns)
+
+        tag = AnnotationWrapper._wrap(None, obj)
+        assert tag.getValue() == text_value.encode('utf8')
+        assert tag.getNs() == ns.encode('utf8')
+
+    def test_file_annotation(self):
+        """Tests AnnotationWrapper methods return strings"""
+        file_name = u'₩€_file_$$'
+        f = OriginalFileI()
+        f.name = rstring(file_name)
+        obj = FileAnnotationI()
+        obj.file = f
+
+        file_ann = AnnotationWrapper._wrap(None, obj)
+        assert file_ann.getFileName() == file_name.encode('utf8')
+
+    def test_map_annotation(self):
+        """Tests MapAnnotationWrapper.getValue() returns unicode"""
+        values = [(u'one', u'₹₹'), (u'two', u'¥¥')]
+        obj = MapAnnotationI()
+        data = [NamedValue(d[0], d[1]) for d in values]
+        obj.setMapValue(data)
+
+        map_ann = AnnotationWrapper._wrap(None, obj)
+        assert map_ann.getValue() == values
+
+    def test_plate(self):
+        """Tests label methods for Plate and Well."""
+        name = u'plate_∞'
+        cols = 4
+        rows = 3
+        obj = PlateI()
+        obj.name = rstring(name)
+
+        plate = PlateWrapper(None, obj)
+        assert plate.getName() == name.encode('utf8')
+        plate._gridSize = {'rows': rows, 'columns': cols}
+        assert plate.getColumnLabels() == [c for c in range(1, cols + 1)]
+        assert plate.getRowLabels() == ['A', 'B', 'C']
+
+        well_obj = WellI()
+        well_obj.column = rint(1)
+        well_obj.row = rint(2)
+
+        class MockWell(WellWrapper):
+            def getParent(self):
+                return plate
+
+        well = MockWell(None, well_obj)
+        assert well.getWellPos() == "C2"
 
 
 class TestBlitzGatewayUnicode(object):
