96b33870d760e92356367abc7728f11f8c22dbd8
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-11-01 08:38:14 +0000

    drop encoding

--- src/omero/util/populate_roi.py
+++ src/omero/util/populate_roi.py
@@ -165,8 +165,7 @@ class DownloadingOriginalFileProvider(object):
         """
         log.info("Downloading original file: %d" % original_file.id.val)
         self.raw_file_store.setFileId(original_file.id.val)
-        temporary_file = tempfile.TemporaryFile(mode='rt+', dir=str(self.dir),
-                                                encoding="utf-8")
+        temporary_file = tempfile.TemporaryFile(mode='rt+', dir=str(self.dir))
         size = original_file.size.val
         for i in range((old_div(size, self.BUFFER_SIZE)) + 1):
             index = i * self.BUFFER_SIZE
