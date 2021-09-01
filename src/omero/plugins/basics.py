cce1eabe897f5a0ac434bee1c513bb9af96c6789
Josh Moore <j.a.moore@dundee.ac.uk>
2021-09-01 13:07:28 +0200

    Use omero-cli tag for feedback.

--- src/omero/plugins/basics.py
+++ src/omero/plugins/basics.py
@@ -169,7 +169,7 @@ Other help topics:
 
 For additional information, see:
 https://docs.openmicroscopy.org/latest/omero/users/cli/index.html
-Report bugs at https://forum.image.sc/
+Report bugs at https://forum.image.sc/tag/omero-cli
 """
 
 
