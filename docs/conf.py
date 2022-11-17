6b1775a30bcf84c3e97a1fe6c7c53842a1ee09c4
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-11-17 14:21:41 +0000

    add source file

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,7 @@ language = 'en'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ".rst"
 
 # Build docs without external dependencies
 autodoc_mock_imports = ["omero", "omero_ext", "past", "future", 
