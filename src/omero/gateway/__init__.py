c0754934a8bee064dc569dabd60a434d9d1af6c9
barrettMCW <103449618+barrettMCW@users.noreply.github.com>
2023-08-07 07:22:10 -0500

    typo

--- src/omero/gateway/__init__.py
+++ src/omero/gateway/__init__.py
@@ -10393,7 +10393,7 @@ class _ImageWrapper (BlitzObjectWrapper, OmeroRestrictionWrapper):
                  filterByCurrentUser is True, otherwise the total rois
                  found.
         """
-        return [omero.gateway.RoiWrapper(self.conn, roi) for roi in 
+        return [RoiWrapper(self.conn, roi) for roi in 
                 self._get_rois(shapeType, filterByCurrentUser)]
         
 
