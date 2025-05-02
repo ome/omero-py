3f0823138691f922de8a3ebc63e7a66ef509ee22
Tom-TBT <tom.boissonnet@hhu.de>
2025-05-02 21:24:39 +0200

    listing annotation support for Roi

--- src/omero/gateway/__init__.py
+++ src/omero/gateway/__init__.py
@@ -154,9 +154,9 @@ def getAnnotationLinkTableName(objecttype):
     if objecttype == "project":
         return "ProjectAnnotationLink"
     if objecttype == "dataset":
-        return"DatasetAnnotationLink"
+        return "DatasetAnnotationLink"
     if objecttype == "image":
-        return"ImageAnnotationLink"
+        return "ImageAnnotationLink"
     if objecttype == "screen":
         return "ScreenAnnotationLink"
     if objecttype == "plate":
@@ -165,6 +165,8 @@ def getAnnotationLinkTableName(objecttype):
         return "PlateAcquisitionAnnotationLink"
     if objecttype == "well":
         return "WellAnnotationLink"
+    if objecttype == "roi":
+        return "RoiAnnotationLink"
     return None
 
 
@@ -5237,7 +5239,7 @@ class AnnotationWrapper (BlitzObjectWrapper):
     def getParentLinks(self, ptype, pids=None):
         ptype = ptype.title().replace("Plateacquisition", "PlateAcquisition")
         objs = ('Project', 'Dataset', 'Image', 'Screen',
-                'Plate', 'Well', 'PlateAcquisition')
+                'Plate', 'Well', 'PlateAcquisition', 'Roi')
         if ptype not in objs:
             raise AttributeError(
                 "getParentLinks(): ptype '%s' not supported" % ptype)
@@ -9491,7 +9493,7 @@ class _ImageWrapper (BlitzObjectWrapper, OmeroRestrictionWrapper):
             p1 = 0
             p2 = 1
             while (p2 <= len(tokens) and
-                   (font.getbbox(' '.join(tokens[p1:p2]))[2] - 
+                   (font.getbbox(' '.join(tokens[p1:p2]))[2] -
                     font.getbbox(' '.join(tokens[p1:p2]))[0]) < width):
                 p2 += 1
             rv.append(' '.join(tokens[p1:p2-1]))
@@ -10407,9 +10409,9 @@ class _ImageWrapper (BlitzObjectWrapper, OmeroRestrictionWrapper):
                  filterByCurrentUser is True, otherwise the total rois
                  found.
         """
-        return [RoiWrapper(self._conn, roi) for roi in 
+        return [RoiWrapper(self._conn, roi) for roi in
                 self._get_rois(shapeType, filterByCurrentUser)]
-        
+
 
     def getROICount(self, shapeType=None, filterByCurrentUser=False):
         """
