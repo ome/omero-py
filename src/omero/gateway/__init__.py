09328ffb95db566dfad18a242afc0acf9bd1fd88
William Moore <w.moore@dundee.ac.uk>
2026-09-21 21:19:02 +0100

    Use getDefaultGroup(self.id).id for experimenter._linkObject()

--- src/omero/gateway/__init__.py
+++ src/omero/gateway/__init__.py
@@ -1059,12 +1059,7 @@ class BlitzObjectWrapper (object):
         if isinstance(self, ExperimenterGroupWrapper):
             ctx.setOmeroGroup(self.id)
         elif isinstance(self, ExperimenterWrapper):
-            # If user is me, save to default group
-            if self._conn.getUserId() == self.id:
-                group_id = self._conn.getEventContext().groupId
-            else:
-                # save link to a group that the user is in
-                group_id = [g.id.val for g in self.copyGroupExperimenterMap()][0]
+            group_id = self._conn.getDefaultGroup(self.id).id
             ctx.setOmeroGroup(group_id)
         else:
             ctx.setOmeroGroup(self.details.group.id.val)
